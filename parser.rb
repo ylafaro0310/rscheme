da3a9fa239dafe648ceaf903d87c4599b3f1b8bb
ylafaro0310 <y16mukurou@gmail.com>
2021-01-14 22:24:13 +0900

    Add 四則演算

--- parser.rb
+++ parser.rb
@@ -1,10 +1,56 @@
-def parser(str="")
+def lex(str="")
     str = str.gsub("("," ( ")
     str = str.gsub(")"," ) ")
     sarray = str.split(" ")
-    for elem in sarray
-        puts elem
+    return sarray
+end
+
+def parser(tokens)
+    list = []
+    for token in tokens do
+        case token
+        when ")" then
+            tmp = []
+            while true do
+                elem = list.pop()            
+                if elem == "(" then 
+                    list.push(tmp)
+                    break 
+                end
+                tmp.unshift(elem)
+            end
+        else
+            list.push(token)
+        end
+    end
+    return list[0]
+end
+
+$define = {}
+$define["+"] = -> (x,y){x + y}
+$define["-"] = -> (x,y){x - y}
+$define["*"] = -> (x,y){x * y}
+$define["/"] = -> (x,y){x / y}
+$define["define"] = -> (x,y){ $define[x] = y}
+#$define["lambda"] = -> (x,y){ (x){y} }
+def eval(list)
+    if list.instance_of?(Array) then        
+        arg1 = eval(list[1])
+        arg2 = eval(list[2])
+        $define[list[0]].call(arg1,arg2)
+    else
+        return list.to_i
     end
 end
 
-parser("(+ 1 2)")
+# 文字列をトークンに分解
+tokens = lex("(+ 3 (- (* 4 2) 1))")
+
+# トークンをリストに変換
+list = parser(tokens)
+
+# (f x y) の評価
+ret = eval(list)
+
+# 結果の表示
+p ret
